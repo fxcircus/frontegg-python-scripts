38a8f0382d5773649abb96e3451e91c8a780dad6
Roy Daniel <56827243+fxcircus@users.noreply.github.com>
2023-10-02 17:33:26 -0400

    Update get_users_with_pagination.py

--- get_users_with_pagination.py
+++ get_users_with_pagination.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import subprocess 
 
 ################################################################
 #  Global variables
@@ -74,29 +73,16 @@ def get_users_with_pagination():
         next = next_page_res.get('_links').get('next')
     return items_arr
 
-# Input ➜ users from Get Users endpoint
-# Output ➜ aray of userIds i.e - ["24289903-fc9f-4971-88b9-ce089651aca9", "16b4483f-4746-4069-9d30-da5f9eb7da03", ...]
-def build_users_param(users_arr):
-    ids = [user['id'] for user in users_arr]  # Extracting 'id' from each JSON
-    id_params = ','.join(ids)  # Joining ids with commas
-    return f"?ids={id_params}"
-
 ################################################################
 #  Run script
 ################################################################
 
 bearer_token = get_vendor_jwt()
 users_arr = get_users_with_pagination()
-users_param = build_users_param(users_arr)
 
 # write results to log file
 with open("result.log", "w") as file:
     file.write(json.dumps(users_arr, indent=4))
     file.write(f"\n\nFinal result:\n\n{get_users_with_pagination}\n")
-
-# Convert the list to string and copy to clipboard
-# filtered_users_with_email_str = str(filtered_users_with_email)
-# subprocess.run("pbcopy", text=True, input=filtered_users_with_email_str)
-# print("\ncopied to clipboard!\n")
 
 print("Users data and final result written to result.log!")
